efcd94acdb3864f0bb8f017caeae4c9928641644
Uuvraj Sahni <uuvraj@users.noreply.github.com>
2021-07-13 23:51:37 +0530

    Initial commit

---
+++ MultipleCaches/src/main/java/io/github/uuvraj/multiplecaches/MultipleCachesApplication.java
@@ -0,0 +1,13 @@
+package io.github.uuvraj.multiplecaches;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipleCachesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultipleCachesApplication.class, args);
+	}
+
+}
---
+++ MultipleCaches/src/test/java/io/github/uuvraj/multiplecaches/MultipleCachesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.uuvraj.multiplecaches;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultipleCachesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
